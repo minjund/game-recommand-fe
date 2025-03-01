dc79f4ad7932355926d3085710a8ef2eecba52ad
minjund <mam060922@naver.com>
2025-03-01 15:42:38 +0900

    fix : 구글 seo 설정

--- src/main.js
+++ src/main.js
@@ -3,20 +3,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { createMetaManager } from 'vue-meta'
+import {createHead} from '@unhead/vue'
 import router from './router'
 import App from './App.vue'
 import * as LucideIcons from 'lucide-vue-next'
 
 const app = createApp(App)
 const pinia = createPinia()
+const head = createHead()
 
 // persistedstate 플러그인 추가
 pinia.use(piniaPluginPersistedstate)
 
-// Meta 관리자 생성 (전역 기본값 설정)
-const metaManager = createMetaManager({
-    // 기본 메타 태그 설정
+// 전역 메타 설정
+head.addHeadObjs({
+    title: 'dotpickplay - 스팀 게임 추천 서비스',
+    titleTemplate: '%s | dotpickplay',
     meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -35,14 +37,12 @@ const metaManager = createMetaManager({
         { name: 'robots', content: 'index, follow' },
         { hid: 'canonical', rel: 'canonical', href: 'https://dotpickplay.com' }
     ],
-    title: 'dotpickplay - 스팀 게임 추천 서비스',
-    titleTemplate: '%s | dotpickplay'
 })
 
-// Pinia, Router, Meta 관리자를 앱에 연결
+// Pinia, Router, Head를 앱에 연결
 app.use(pinia)
 app.use(router)
-app.use(metaManager)
+app.use(head)
 
 // Lucide 아이콘들을 전역 컴포넌트로 등록
 for (const [key, component] of Object.entries(LucideIcons)) {
